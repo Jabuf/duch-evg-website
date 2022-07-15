efb2873dc2b9a8cd3555b9e44d0487837e1b9869
Jabuf <jason.buffet@gmail.com>
2022-07-15 21:08:37 +0200

    fix z-index issue with the dropdown menu in mobile

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col h-screen justify-between">
-    <DuchNavBar class="h-14"/>
-    <router-view class="mb-auto px-5 sm:px-20 sm:py-20"/>
+    <DuchNavBar class="h-14 z-10"/>
+    <router-view class="mb-auto px-5 sm:px-20 sm:py-20 z-0"/>
   </div>
 </template>
 
--- src/components/section/DuchNavBar.vue
+++ src/components/section/DuchNavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
+  <Disclosure v-slot="{ open }" as="nav" class="bg-gray-800">
     <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
       <div class="relative flex items-center justify-between h-16">
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -7,36 +7,37 @@
           <DisclosureButton
               class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
             <span class="sr-only">Open main menu</span>
-            <MenuIcon v-if="!open" class="block h-6 w-6" aria-hidden="true"/>
-            <XIcon v-else class="block h-6 w-6" aria-hidden="true"/>
+            <MenuIcon v-if="!open" aria-hidden="true" class="block h-6 w-6"/>
+            <XIcon v-else aria-hidden="true" class="block h-6 w-6"/>
           </DisclosureButton>
         </div>
         <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
           <div class="flex-shrink-0 flex items-center">
             <a href="./">
-              <img class="block lg:hidden h-8 w-auto" src="/logo.png"
-                   alt="Workflow"/>
-              <img class="hidden lg:block h-8 w-auto"
-                   src="/logo.png" alt="Workflow"/>
+              <img alt="Workflow" class="block lg:hidden h-8 w-auto"
+                   src="/logo.png"/>
+              <img alt="Workflow"
+                   class="hidden lg:block h-8 w-auto" src="/logo.png"/>
             </a>
           </div>
           <div class="hidden sm:block sm:ml-6">
             <div class="flex space-x-4">
-              <a v-for="item in navigation" :key="item.name" :href="item.href"
-                 @click="setCurrent(item.href)"
+              <a v-for="item in navigation" :key="item.name" :aria-current="item.current ? 'page' : undefined"
                  :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'px-3 py-2 rounded-md text-sm font-medium']"
-                 :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+                 :href="item.href"
+                 @click="setCurrent(item.href)">{{ item.name }}</a>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <DisclosurePanel class="sm:hidden">
+    <DisclosurePanel class="sm:hidden bg-white">
       <div class="px-2 pt-2 pb-3 space-y-1">
-        <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href"
-                          @click="setCurrent(item.href)"
+        <DisclosureButton v-for="item in navigation" :key="item.name" :aria-current="item.current ? 'page' : undefined"
                           :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block px-3 py-2 rounded-md text-base font-medium']"
-                          :aria-current="item.current ? 'page' : undefined">{{ item.name }}
+                          :href="item.href"
+                          as="a"
+                          @click="setCurrent(item.href)">{{ item.name }}
         </DisclosureButton>
       </div>
     </DisclosurePanel>
