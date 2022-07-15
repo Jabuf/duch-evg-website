5da9070a53cd47b05f9b58f49cfe12b6fa760ec6
Jabuf <jason.buffet@gmail.com>
2022-07-15 17:39:09 +0200

    data: add some pictures

---
+++ src/components/PicturesWidget.vue
@@ -0,0 +1,17 @@
+<template>
+  <PictureWidget :name="state.pictureNumber"/>
+</template>
+
+<script setup>
+import {onMounted, reactive} from 'vue'
+import PictureWidget from '@/components/PictureWidget.vue'
+import {getRandomNumberString} from '@/utils/math.js'
+
+const state = reactive({pictureNumber: getRandomNumberString(1, 5)})
+
+onMounted(() => {
+  window.setInterval(() => {
+    state.pictureNumber = '' + getRandomNumberString(1, 5)
+  }, 5000)
+})
+</script>
