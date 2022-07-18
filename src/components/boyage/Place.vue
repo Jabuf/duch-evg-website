6e934ca34bdca1f15132ba402845ea60892901ed
Jabuf <jason.buffet@gmail.com>
2022-07-18 21:17:03 +0200

    fix the position of the map button

--- src/components/boyage/Place.vue
+++ src/components/boyage/Place.vue
@@ -1,24 +1,20 @@
 <template>
   <div>
-    <div v-if="place.location">
-      <a :href="place.location" target="_blank">
-        <UIcon :path="mdiMapMarker" class="flex" size="24"/>
+    <div
+        class="flex items-center px-5 py-3 border shadow-sm font-medium text-sm rounded-lg bg-gray-700 cursor-pointer"
+        @click="displayComment()">
+      <a v-if="place.location" :href="place.location" target="_blank">
+        <UIcon :path="mdiMapMarker" class="flex text-red-700" size="24"/>
       </a>
+      <div class="w-full px-5 align-middle text-white">{{ place.order + ' - ' + place.name }}
+      </div>
+      <div class="right-0">
+        <UIcon :class="state.rotate?'inline-flex rotate-90':''" :path="mdiPlay" :size="24"
+               class="align-middle text-white"/>
+      </div>
     </div>
-    <div class="h-full">
-      <div
-          class="flex items-center px-5 py-3 border shadow-sm font-medium text-sm rounded-lg bg-gray-700 cursor-pointer"
-          @click="displayComment()">
-        <div class="w-full px-5 align-middle text-white">{{ place.order + ' - ' + place.name }}
-        </div>
-        <div class="right-0">
-          <UIcon :class="state.rotate?'inline-flex rotate-90':''" :path="mdiPlay" :size="24"
-                 class="align-middle text-white"/>
-        </div>
-      </div>
-      <div v-if="state.rotate" class="px-5">
-        <Reviews :place-number="place.order"/>
-      </div>
+    <div v-if="state.rotate" class="px-5">
+      <Reviews :place-number="place.order"/>
     </div>
   </div>
 </template>
