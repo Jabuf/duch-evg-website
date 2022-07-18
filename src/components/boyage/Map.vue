19aefee98c33d9328f85ef5981a765f0b0cc3585
Jabuf <jason.buffet@gmail.com>
2022-07-18 20:36:06 +0200

    change the embedded map

--- src/components/boyage/Map.vue
+++ src/components/boyage/Map.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="py-5 h-screen">
-    <iframe class="h-3/5 w-full"
-            src="https://www.google.com/maps/d/u/0/embed?mid=1cWxytGIY5jgN4Z2sg6bHsDgxRj_yWiY&ehbc=2E312F"/>
+    <iframe
+        allowfullscreen=""
+        class="h-3/5 w-full" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
+        src="https://www.google.com/maps/embed?pb=!1m34!1m12!1m3!1d3004.7168662852455!2d-8.612278083719119!3d41.14070642928759!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m19!3e2!4m5!1s0xd2464dfb7625aa5%3A0x309fce5fc580e887!2sCaves%20C%C3%A1lem%2C%20Av.%20de%20Diogo%20Leite%20344%2C%204400-111%20Vila%20Nova%20de%20Gaia%2C%20Portugal!3m2!1d41.1381447!2d-8.6109182!4m5!1s0xd2464e75cfd46d3%3A0xc25b40cf99456aab!2sPonte%20Lu%C3%ADs%20I%2C%20Ponte%20Luiz%20I%2C%20Porto%2C%20Portugal!3m2!1d41.139958899999996!2d-8.6094485!4m5!1s0xd2464e724adc789%3A0xa72dd2f0f7d62f0!2sRibeira%2C%20Porto%2C%20Portugal!3m2!1d41.1432682!2d-8.6103135!5e0!3m2!1sen!2sfr!4v1658169173528!5m2!1sen!2sfr"
+        style="border:0;"></iframe>
   </div>
 </template>
 
